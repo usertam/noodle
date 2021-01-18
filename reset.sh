67078a6f4e2e7bfb12807795a87df6534b74f82e
usertam <heiyiutam@gmail.com>
2021-01-18 22:11:14 +0800

    reset.sh: tweaks

--- reset.sh
+++ reset.sh
@@ -2,10 +2,11 @@
 set -e
 
 # cd to project root
-cd "$(dirname $0)/../"
+cd "$(dirname $0)"
 
 # delete fetch list and files
-rm -rf downloads fetch.txt
+rm -rf downloads fetch.txt mail/body.html
+mkdir -p downloads
 
 # delete records also if specified
 if [ "$1" = "records" ]; then
