34e5359a64b93555ab0e5c7c79aa19a2bf6d37f8
usertam <heiyiutam@gmail.com>
2021-09-01 21:29:24 +0800

    noodle.py: display download link labels for folder items

--- noodle.py
+++ noodle.py
@@ -164,7 +164,8 @@ class Site:
                     for file_code, file in enumerate(module.folder, start=file_code):
                         name = unquote(os.path.basename(file))
                         code = f's{sec_code}-{mod_code}-f{file_code}'
-                        f.write(f'  - [{name}][{code}]\n')
+                        f.write(' ' * 2 + f'- [{name}][{code}]\n')
+                        f.write(' ' * 6 + f'<file>DL: [{name}][{code}]</file>\n')
 
                 # write newline if no modules are written
                 if mod_code > 0:
@@ -354,7 +355,8 @@ class Diff:
                     for file_code, file in enumerate(module.folder, start=file_code):
                         name = unquote(os.path.basename(file))
                         code = f's{sec_code}-{mod_code}-f{file_code}'
-                        f.write(f'  - [{prefix}{name}{suffix}][{code}]\n')
+                        f.write(' ' * 2 + f'- [{prefix}{name}{suffix}][{code}]\n')
+                        f.write(' ' * 6 + f'<file>DL: [{prefix}{name}{suffix}][{code}]</file>\n')
 
                 # write newline if no modules are written
                 if mod_code > 0:
